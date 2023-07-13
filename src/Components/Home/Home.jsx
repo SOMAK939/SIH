7dade8518b249093972cd1bcede6d9e6d641ac04
mrinnnmoy <mrinmoy202000@gmail.com>
2023-07-13 09:58:45 +0530

    build-home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import "./Home.scss";
+import Video from "../../assets/video.mp4";
+import Plane from "../../assets/takeoff.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home flex container'>
+      <div className="mainText">
+        <h1>Create Ever-Lasting Memories With Us</h1>
+      </div>
+
+      <div className="homeImages flex">
+        <div className="videoDiv">
+          <video src={Video} autoPlay muted loop className='video'></video>
+        </div>
+
+        <img src={Plane} alt="plane" className='plane' />
+      </div>
+    </div>
   )
 };
 
